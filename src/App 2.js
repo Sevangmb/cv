1e11a8dd081f65ace37cfd4cc7ed54f62ae6e17e
Sevangmb <sevangm@gmail.com>
2020-09-11 00:00:08 +0200

    ok

---
+++ src/App 2.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
+import Competences from './components/Competences'
+import Exemple2 from './components/Exemple2'
+import data from "./data"; 
+import CenteredTabs from './components/menu';
+import Home from './components/home';
+
+const App = () => {
+  const [currentTab, setCurrentTab] = useState(0); 
+
+  const onChangeTab = (event, newValue) => {
+      setCurrentTab(newValue);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <CenteredTabs current={currentTab} onChange={onChangeTab} />
+
+      {currentTab === 1 && (<Competences />)}
+      {currentTab === 2 && (<Exemple2 title="Expériences" experiences={data.Experiences} />)}
+      {currentTab === 0 && (<Home />)}
+
+      
+    </ThemeProvider>
+  )
+};
+
+export default App
---
+++ src/theme 2.js
@@ -0,0 +1,18 @@
+import { createMuiTheme }  from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+
+  palette: {
+    background: {
+      default: "#f4f6ff"
+    },
+    primary: {
+      main: '#4f8a8b',
+    },
+    secondary: {
+      main: '#ffcb74',
+    },
+  },
+});
+
+export default theme
