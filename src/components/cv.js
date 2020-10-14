83fd5db59960e884017344b2f04e9a08cad003d6
Sevangmb <Sevans@hotmail.fr>
2020-10-14 11:16:59 +0200

    Update cv.js

--- src/components/cv.js
+++ src/components/cv.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Exemple2 from './Exemple2';
+import Exemple2 from './Exemple3';
+import Exemple2 from './Exemple1';
  
 class Cv extends Component {
     render() {
